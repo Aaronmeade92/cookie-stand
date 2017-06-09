7f7b9b06a2287b463ad998abc499de24cf581fad
Aaron <aaronmeade9292@gmail.com>
2017-06-08 18:16:44 -0700

    staring js

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var FirstAndPike [
+  'Minimum Customers per Hour',
+  'Maximum Customers per Hour',
+  'Average Cookies per Customer'
+];
+
+
+
+var SeaTacAirport [
+  'Minimum Customers per Hour',
+  'Maximum Customers per Hour',
+  'Average Cookies per Customer'
+];
+
+
+var SeattleCenter [
+  'Minimum Customers per Hour',
+  'Maximum Customers per Hour',
+  'Average Cookies per Customer'
+];
+
+
+var CapitolHill [
+  'Minimum Customers per Hour',
+  'Maximum Customers per Hour',
+  'Average Cookies per Customer'
+];
+
+
+var Alki [
+  'Minimum Customers per Hour',
+  'Maximum Customers per Hour',
+  'Average Cookies per Customer'
+];
