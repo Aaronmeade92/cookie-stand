f6eaabe1872cb0b94455a5ca8732fc064b4af65f
Aaron <aaronmeade9292@gmail.com>
2017-06-08 18:36:45 -0700

    ACP

--- app.js
+++ app.js
@@ -6,13 +6,30 @@ var FirstAndPike [
   'Average Cookies per Customer'
 ];
 
+var FirstAndPikeObject {
+  0: 'Minimum Customers per Hour',
+  1: 'Maximum Customers per Hour',
+  2: 'Average Cookies per Customer'
+};
 
+var FirstAndPike {
+  MinCustomers: 23;
+  MaxCustomers: 65;
+  AvgCookieSales: 6.3;
+}
+console.log('The average number of cookies sold per customer is' + AvgCookieSales)
 
 var SeaTacAirport [
   'Minimum Customers per Hour',
   'Maximum Customers per Hour',
   'Average Cookies per Customer'
 ];
+
+var SeaTacAirportObject {
+  0: 'Minimum Customers per Hour',
+  1: 'Maximum Customers per Hour',
+  2: 'Average Cookies per Customer'
+};
 
 
 var SeattleCenter [
@@ -21,6 +38,11 @@ var SeattleCenter [
   'Average Cookies per Customer'
 ];
 
+var SeattleCenterObject {
+  0: 'Minimum Customers per Hour',
+  1: 'Maximum Customers per Hour',
+  2: 'Average Cookies per Customer'
+};
 
 var CapitolHill [
   'Minimum Customers per Hour',
@@ -28,9 +50,21 @@ var CapitolHill [
   'Average Cookies per Customer'
 ];
 
+var CapitolHillObject {
+  0: 'Minimum Customers per Hour',
+  1: 'Maximum Customers per Hour',
+  2: 'Average Cookies per Customer'
+};
+
 
 var Alki [
   'Minimum Customers per Hour',
   'Maximum Customers per Hour',
   'Average Cookies per Customer'
 ];
+
+var AlkiObject {
+  0: 'Minimum Customers per Hour',
+  1: 'Maximum Customers per Hour',
+  2: 'Average Cookies per Customer'
+};
